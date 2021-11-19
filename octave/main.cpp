912d99126286d18b517c67384cc619f6338c23ba
upuaut <46531111+upuaut-wq@users.noreply.github.com>
2021-11-19 18:22:20 -0300

    Exemplo funcional OCTAVE

--- octave/main.cpp
+++ octave/main.cpp
@@ -7,78 +7,69 @@
 #include <octave/octave.h>
 #include <octave/parse.h>
 #include <octave/interpreter.h>
+#include <octave-5.2.0/octave/octave-default-image.h>
 
-int
-main (void)
+int main(void)
 {
   // Create interpreter.
 
   octave::interpreter interpreter;
 
   try
+  {
+    int status = interpreter.execute();
+
+    if (status != 0)
     {
-      // Inhibit reading history file by calling
-      //
-      //   interpreter.initialize_history (false);
-
-      // Set custom load path here if you wish by calling
-      //
-      //   interpreter.initialize_load_path (false);
-
-      // Perform final initialization of interpreter, including
-      // executing commands from startup files by calling
-      //
-      //   interpreter.initialize ();
-      //
-      //   if (! interpreter.initialized ())
-      //     {
-      //       std::cerr << "Octave interpreter initialization failed!"
-      //                 << std::endl;
-      //       exit (status);
-      //     }
-      //
-      // You may skip this step if you don't need to do anything
-      // between reading the startup files and telling the interpreter
-      // that you are ready to execute commands.
-
-      // Tell the interpreter that we're ready to execute commands:
-
-      int status = interpreter.execute ();
-
-      if (status != 0)
-        {
-          std::cerr << "creating embedded Octave interpreter failed!"
-                    << std::endl;
-          return status;
-        }
-
-      octave_idx_type n = 2;
-      octave_value_list in;
-
-      for (octave_idx_type i = 0; i < n; i++)
-        in(i) = octave_value (5 * (i + 2));
-
-      octave_value_list out = octave::feval ("gcd", in, 1);
-
-      if (out.length () > 0)
-        std::cout << "GCD of ["
-                  << in(0).int_value ()
-                  << ", "
-                  << in(1).int_value ()
-                  << "] is " << out(0).int_value ()
-                  << std::endl;
-      else
-        std::cout << "invalid\n";
+      std::cerr << "creating embedded Octave interpreter failed!"
+                << std::endl;
+      return status;
     }
-  catch (const octave::exit_exception& ex)
-    {
-      std::cerr << "Octave interpreter exited with status = "
-                << ex.exit_status () << std::endl;
-    }
-  catch (const octave::execution_exception&)
-    {
-      std::cerr << "error encountered in Octave evaluator!" << std::endl;
-    }
+
+    //Lista de argumentos para o comando pkg
+    octave_value_list load_p;
+    load_p(0) = octave_value("load"); //Argumento load
+    load_p(1) = octave_value("image"); //Argumento image(Pacote)
+
+    octave::feval("pkg", load_p); //Chama o comando pkg com a lista de argumentos
+  
+    //Cria um lista de valores
+    octave_value_list in;
+    in(0) = octave_value("rice.jpg"); //Adiciona na lista de argumentos o nome da imagem na posição 1 da lista
+
+    //Cria outra lista de saida
+    octave_value_list out;
+    out(0) = octave_value(octave::feval("imread", in(0), 1)); //Executa o comando imread com o a lista de argumentos
+
+    //Cria outra lista de valores
+    octave_value_list in2;
+
+    //Cria a lista de argumentos para salvar a imagem
+    in2(1) = octave_value("image");
+    in2(2) = octave_value("png");
+
+    //Cria uma nova lista
+    octave_value_list in3;
+    in3(0) = octave_value(out(0));
+    out(3) = octave_value(octave::feval("imhist", in3)); //Executa o comando imadjust com a lista de argumentos
+    in2(0) = octave_value(out(3));
+
+    octave::feval("imwrite", in2, 2);//Salva a imagem com a lista de argumentos n2
+
+    if (out.length() > 0)
+      std::cout << "OK" << std::endl;
+    else
+      std::cout << "invalid\n";
+  }
+  catch (const octave::exit_exception &ex)
+  {
+    std::cerr << "Octave interpreter exited with status = "
+              << ex.exit_status() << std::endl;
+  }
+  catch (const octave::execution_exception &)
+  {
+    std::cerr << "error encountered in Octave evaluator!" << std::endl;
+  }
 
   return 0;
 }
