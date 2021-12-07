10940ace4d42743b9602523c0371f77f9e49ab46
Leonardo de Jesus Queiroz <upuaut@tutanota.com>
2021-12-07 08:51:24 -0300

    at

--- octave/main.cpp
+++ octave/main.cpp
@@ -35,26 +35,29 @@ int main(void)
   
     //Cria um lista de valores
     octave_value_list in;
-    in(0) = octave_value("rice.jpg"); //Adiciona na lista de argumentos o nome da imagem na posição 1 da lista
+    in(0) = octave_value("md.png"); //Adiciona na lista de argumentos o nome da imagem na posição 1 da lista
 
     //Cria outra lista de saida
     octave_value_list out;
-    out(0) = octave_value(octave::feval("imread", in(0), 1)); //Executa o comando imread com o a lista de argumentos
+    out(0) = octave_value(octave::feval("imread", in)); //Executa o comando imread com o a lista de argumentos
 
-    //Cria outra lista de valores
-    octave_value_list in2;
+    out(0) = octave_value(octave::feval("rgb2gray",out));
+    //octave value list
+    octave_value_list imb2w;
+    imb2w(0) = octave_value(out(0));
+
+    //Não funciona
+    out(0) = octave_value(octave::feval("im2bw",octave_value(0.5)));
+  
+
+
 
     //Cria a lista de argumentos para salvar a imagem
-    in2(1) = octave_value("image");
-    in2(2) = octave_value("png");
+    out(1) = octave_value("imagess22");
+    out(2) = octave_value("png");
 
-    //Cria uma nova lista
-    octave_value_list in3;
-    in3(0) = octave_value(out(0));
-    out(3) = octave_value(octave::feval("imhist", in3)); //Executa o comando imadjust com a lista de argumentos
-    in2(0) = octave_value(out(3));
 
-    octave::feval("imwrite", in2, 2);//Salva a imagem com a lista de argumentos n2
+    octave::feval("imwrite", out);//Salva a imagem com a lista de argumentos n2
 
     if (out.length() > 0)
       std::cout << "OK" << std::endl;
